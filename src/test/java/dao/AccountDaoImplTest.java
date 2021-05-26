528186e533b6221993164aac3438a87f2eb987b1
Eduard Khabukhaev <ludoedik4@gmail.com>
2021-05-26 17:52:04 +0700

    Some test preparations

--- src/test/java/dao/AccountDaoImplTest.java
+++ src/test/java/dao/AccountDaoImplTest.java
@@ -1,7 +1,36 @@
 package dao;
 
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.shortThat;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@ExtendWith(MockitoExtension.class)
 public class AccountDaoImplTest {
+    @Mock
+    ResultSet testSet;
+
+    @Test
+    public void readBalanceTest() throws SQLException {
+        assertNotNull(testSet);
+        Mockito.when(testSet.next()).thenReturn(true).thenReturn(false);
+        Mockito.when(testSet.getInt("ID")).thenReturn(1);
+        Mockito.when(testSet.getInt("CLIENT_ID")).thenReturn(1);
+        Mockito.when(testSet.getBigDecimal("CURRENCY")).thenReturn(BigDecimal.valueOf(410));
+        //new AccountDaoImpl().readBalance();
+    }
 
 }
